8424955a364efd15113e16f71f9bd255947b7555
annienf17 <a.ulanskaxxi@gmail.com>
2024-09-17 14:42:16 +0100

    isDuplicate update

--- src/features/contacts/contactsSlice.js
+++ src/features/contacts/contactsSlice.js
@@ -17,16 +17,25 @@ const loadInitialContacts = () => {
 };
 
 const isDuplicate = (contacts, newContact) => {
+  const duplicateContact = contacts.find(
+    (contact) =>
+      contact.name.toLowerCase() === newContact.name.toLowerCase() &&
+      contact.number === newContact.number
+  );
+
+  if (duplicateContact) {
+    return "This name and number already exist.";
+  }
+
   const duplicateName = contacts.some(
     (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
   );
+
   const duplicateNumber = contacts.some(
     (contact) => contact.number === newContact.number
   );
 
-  if (duplicateName && duplicateNumber) {
-    return "This name and number already exist.";
-  } else if (duplicateName) {
+  if (duplicateName) {
     return "This name already exists.";
   } else if (duplicateNumber) {
     return "This number already exists.";
